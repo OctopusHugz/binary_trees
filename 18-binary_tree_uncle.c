13b798cb78d7edbd384eeaaf0dc15d9fc968a5cc
Gabriel <gabrielabdul1@gmail.com>
2020-07-21 17:16:11 +0000

    initial commit of task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * *binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!binary_tree_sibling(node->parent))
+		return (NULL);
+	else
+		return (binary_tree_sibling(node->parent));
+	
+}
+/**
+ * *binary_tree_sibling - function that finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	/* if node we're at is left node */
+	if (node->parent->left->n == node->n)
+		return (node->parent->right);
+	/* if node we're at is right node */
+	else if (node->parent->right->n == node->n)
+		return (node->parent->left);
+	return (NULL);
+}
